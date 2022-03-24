1c553b97e51689c9d3e93a87db33db3dcf5bd8f4
Katherine Li <ysli@shoalter.com>
2022-03-24 14:14:58 +0800

    todo sample

---
+++ todo/src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'todo';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { description: 'eat', done: true },
+    { description: 'sleep', done: false },
+    { description: 'play', done: false },
+    { description: 'laugh', done: false },
+    { description: 'work', done: false },
+  ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter(item => this.filter === 'done' ? item.done : !item.done);
+  }
+
+  addItem(description: any) {
+    // unshift add to front
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+
+  remove(item:any) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
+
+}
